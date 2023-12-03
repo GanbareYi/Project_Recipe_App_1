1f97ee0991dec9d72949b4db34ced8b07f7dcfa9
Yi <wuyikm@gmail.com>
2023-12-02 23:05:41 -0800

    Add 'required' property in input tag.

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -37,19 +37,19 @@ function RecipeCreate({onCreate}) {
         <tbody>
           <tr>
             <td>
-              <input name="name" placeholder="Name" onChange={handleChange} value={formData.name} /> 
+              <input name="name" placeholder="Name" onChange={handleChange} value={formData.name} required/> 
             </td>
             <td>
-              <input name="cuisine" placeholder="Cuisine" onChange={handleChange} value={formData.cuisine} />
+              <input name="cuisine" placeholder="Cuisine" onChange={handleChange} value={formData.cuisine} required/>
             </td>
             <td>
-              <input name="photo" placeholder="URL" onChange={handleChange} value={formData.photo} />
+              <input name="photo" placeholder="URL" onChange={handleChange} value={formData.photo} required/>
             </td>
             <td>
-              <textarea name="ingredients" placeholder="Ingredients" onChange={handleChange} value={formData.ingredients} />
+              <textarea name="ingredients" placeholder="Ingredients" onChange={handleChange} value={formData.ingredients} required/>
             </td>
             <td>
-              <textarea name="preparation" placeholder="Preparation" onChange={handleChange} value={formData.preparation} />
+              <textarea name="preparation" placeholder="Preparation" onChange={handleChange} value={formData.preparation} required/>
             </td>
             <td>
               <button type="submit">Create</button>
